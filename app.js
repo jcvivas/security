a3ac39ffdc9e2b97a3474e15d6a7cc9cdeabaf49
jcvivas <jcvivas@espol.edu.ec>
2024-08-02 15:57:14 -0500

    enrutador a token

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var authorizationSession = require('./middleware/authorization_session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ticketRouter = require('./routes/token');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', authenticateSession, authorizationSession, usersRouter);
+app.use('/token', authenticateSession, authorizationSession, ticketRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
